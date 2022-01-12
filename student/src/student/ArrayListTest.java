f2a25ed714d4586f3d147a97577f8b5ca5574218
pancake <834430775@qq.com>
2022-01-12 22:29:19 +0800

    修改新增学生学号重复和删除学生信息不存在问题

--- student/src/student/ArrayListTest.java
+++ student/src/student/ArrayListTest.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ArrayListTest {
-    ArrayList<Students> array = new ArrayList<Students>();
+    ArrayList<Students> array = new ArrayList<>();
     String[] Menu = new String[]{"1.添加学生", "2.删除学生", "3.修改学生", "4.查看所有学生", "5.退出"};
     Scanner sc = new Scanner(System.in);
     int choice;
+    int index = -1;
+    boolean canUsed = true;
 
     public static void main(String[] args) {
         ArrayListTest arrayListTest = new ArrayListTest();
@@ -22,12 +24,14 @@ public class ArrayListTest {
             System.out.println(Menu[i]);
         }
 
-        Students s1 = new Students("风清扬", 66);
-        Students s2 = new Students("逍遥子", 55);
-        Students s3 = new Students("令狐冲", 22);
-        array.add(s1);
-        array.add(s2);
-        array.add(s3);
+        Students s1 = new Students("风清扬", 66, "ali001");
+        Students s2 = new Students("逍遥子", 55, "ali002");
+        Students s3 = new Students("令狐冲", 22, "ali006");
+        if (array.size() == 0) {
+            array.add(s1);
+            array.add(s2);
+            array.add(s3);
+        }
         System.out.println("请输入你的选择：");
         choice = sc.nextInt();
 
@@ -47,41 +51,79 @@ public class ArrayListTest {
     }
 
     public void addStudent() {
-        Scanner sc = new Scanner(System.in);
+        String sid;
+        while (true) {
+            System.out.println("请输入学生学号：");
+            sid = sc.next();
+            searchStudent(sid);
+            System.out.println("what the fuck" + canUsed);
+            if (canUsed) {
+                break;
+            } else {
+                System.out.println("该学号已存在，请重新输入");
+            }
+        }
         System.out.println("请输入学生姓名：");
         String name = sc.next();
         System.out.println("请输入学生年龄：");
         int age = sc.nextInt();
 
-        Students s = new Students(name, age);
+        Students s = new Students(name, age, sid);
         array.add(s);
-        printCircle();
+        System.out.println("添加成功");
+
+        inMenu();
     }
 
     public void deleteStudent() {
-        printCircle();
-        System.out.println("请输入要删除学生的序号");
-        int index = sc.nextInt();
-        array.remove(index);
-        printCircle();
+        while(true){
+            System.out.println("请输入要删除学生的学号");
+            String sidIndex = sc.next();
+            searchStudent(sidIndex);
+            if(canUsed){
+                System.out.println("该信息不存在，请重新输入");
+            }else{
+                array.remove(index);
+                System.out.println("删除成功");
+                break;
+            }
+        }
+
+        inMenu();
     }
 
-    public void updateStudent(){
-        printCircle();
-        System.out.println("请输入要修改学生的序号");
-        int index = sc.nextInt();
+    public void updateStudent() {
+
         System.out.println("请输入修改后学生的姓名");
         String name = sc.next();
         System.out.println("请输入修改后学生的年龄");
         int age = sc.nextInt();
+        System.out.println("请输入修改后学生的学号");
+        String sid = sc.next();
         array.get(index).setName(name);
         array.get(index).setAge(age);
-        printCircle();
+        array.get(index).setSid(sid);
+        System.out.println("修改成功");
+        inMenu();
     }
 
     public void printCircle() {
         for (int i = 0; i < array.size(); i++) {
-            System.out.println(i + "." + array.get(i).getName() + ", " + array.get(i).getAge());
+            System.out.println(array.get(i).getName() + ", " + array.get(i).getAge() + "," + array.get(i).getSid());
+        }
+        inMenu();
+    }
+
+    public void searchStudent(String sid) {
+        for (int i = 0; i < array.size(); i++) {
+            if (array.get(i).getSid().equals(sid)) {
+                index = i;
+                canUsed = false;
+                break;
+            }else{
+                canUsed = true;
+                index = -1;
+            }
         }
     }
 }
--- student/src/student/Students.java
+++ student/src/student/Students.java
@@ -3,10 +3,12 @@ package student;
 public class Students {
     private String name;
     private int age;
+    private String sid;
 
-    public Students(String name, int age){
+    public Students(String name, int age, String sid){
         this.name = name;
         this.age = age;
+        this.sid = sid;
     }
 
     public void setName(String name){
@@ -17,11 +19,19 @@ public class Students {
         this.age = age;
     }
 
+    public void setSid(String sid){
+        this.sid = sid;
+    }
+
     public String getName() {
         return name;
     }
 
     public int getAge() {
         return age;
+    }
+
+    public String getSid(){
+        return sid;
     }
 }
